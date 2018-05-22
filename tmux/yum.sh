49743df933a92fa4b45e75cc41161eddc41c9df0
Robin Ridler <robin.ridler@gmail.com>
2018-05-22 12:28:25 +0100

    Track why I am installing some packages

---
+++ tmux/yum.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yum install -y --enablerepo mcepl-vim8 \
+               --enablerepo ol7_developer_nodejs8 \
+               --enablerepo ol7_developer_EPEL \
+               --enablerepo flatcap-neomutt \
+                `cat /root/pkgs.d/*`
