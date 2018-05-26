cfc0e6d8a9918586c695a0bcb763dcb73f070e07
Robin Ridler <robin.ridler@gmail.com>
2018-05-26 10:52:06 +0100

    Install python 3

--- tmux/yum.sh
+++ tmux/yum.sh
@@ -4,4 +4,6 @@ yum install -y --enablerepo mcepl-vim8 \
                --enablerepo ol7_developer_nodejs8 \
                --enablerepo ol7_developer_EPEL \
                --enablerepo flatcap-neomutt \
+               --enablerepo ol7_software_collections \
+               --enablerepo ol7_optional_latest \
                 `cat /root/pkgs.d/*`
